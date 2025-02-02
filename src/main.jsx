08f12cab85a7b288d96023931c5be978322845d5
Alexandra0707 <alex07marta@gmail.com>
2025-02-02 15:31:53 +0000

    jfjjf

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { store } from './redux/store';
-import App from './components/App';
+import App from './components/App/App';
 import './index.css';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { PersistGate } from 'redux-persist/integration/react';
